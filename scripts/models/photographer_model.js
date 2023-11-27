f2b7397ac3716194fae2134e3244fb9d6b3284bb
samakunchan <badgehades@hotmail.com>
2023-11-27 19:58:05 +0100

    feat: factory design
    - Utilisation du pattern factory
    - Création d'un fichier json fake
    - Passage de tout le code JS en class + refacto
    - Création des models
    - Création d'un service pour gérer la création des cards
    - Création d'un fichier utils.js pour les variables et fonctions par défaut

---
+++ scripts/models/photographer_model.js
@@ -0,0 +1,35 @@
+export class PhotographerModel {
+    constructor({name, id, city, country, tagline, price, portrait}) {
+        this._name = name;
+        this._id = id;
+        this._city = city;
+        this._country = country;
+        this._tagline = tagline;
+        this._price = price;
+        this._portrait = portrait;
+    }
+
+    get id () {
+        return this._id;
+    }
+
+    get name () {
+        return this._name;
+    }
+
+    get portrait () {
+        return `assets/photographers/${this._portrait}`;
+    }
+
+    get tagline () {
+        return this._tagline;
+    }
+
+    get price () {
+        return `${this._price}€/jour`;
+    }
+
+    get location () {
+        return `${this._country}/${this._city}`;
+    }
+}
