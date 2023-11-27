b4c368d57d1a5ebeea79ca987a7f0247906babfc
samakunchan <badgehades@hotmail.com>
2023-11-27 19:54:57 +0100

    feat: clean
    - Correction sur les fichiers html

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,12 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+const displayModal = () => {
+    const modal = document.getElementById('contact_modal');
+	modal.style.display = 'block';
 }
 
-function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+const closeModal = () => {
+    const modal = document.getElementById('contact_modal');
+    modal.style.display = 'none';
 }
+
+document.getElementById('openModal').addEventListener('click', displayModal);
+document.getElementById('closeModal').addEventListener('click', closeModal);
